6d7aa2dcc37a5f8976356f5044ed4d793779135a
adrianw1994 <189809@edu.p.lodz.pl>
2016-09-03 21:19:39 +0200

    Added topNBestRatedMoviesOfActor and getFirstElements methods in TopRatedMoviesOfActor class.

--- src/main/java/ZZPJ/Project/TopRatedMoviesOfActor.java
+++ src/main/java/ZZPJ/Project/TopRatedMoviesOfActor.java
@@ -22,9 +22,22 @@ public class TopRatedMoviesOfActor {
         sortedMovies.addAll(movies);
         Collections.sort(sortedMovies, movieRatingValueComparator);
         return sortedMovies;
-
     }
 
+    public List<Movie> topNBestRatedMoviesOfActor(int count, List<Movie> movies){
+        movies = sortMoviesByRatingValue(movies);
+        if (movies.size() > count) {
+            movies = getNFirstElements(count, movies);
+        }
+        return movies;
+    }
 
+    public List getNFirstElements(int n, List list){
+        List list2 = new ArrayList();
+        list2.addAll(list);
+        for(int i = list.size(); i > n; i--){
+            list2.remove(i-1);
+        }
+        return list2;
+    }
 }
-
--- src/test/java/ZZPJ/Project/TopRateMoviesOfActorTest.java
+++ src/test/java/ZZPJ/Project/TopRateMoviesOfActorTest.java
@@ -1,6 +1,7 @@
 package ZZPJ.Project;
 
 import ZZPJ.Project.Model.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -9,35 +10,57 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TopRateMoviesOfActorTest {
+    Movie movie1 = new MovieBasic();
+    Movie movie2 = new MovieWithRating(movie1);
+    Movie movie3 = new MovieWithRating(movie1);
+    Movie movie7 = new MovieWithRating(movie1);
+    Movie movie4 = new MovieWithGenres(movie7);
+    Movie movie8 = new MovieWithGenres(movie1);
+    Movie movie5 = new MovieWithRating(movie8);
+    Movie movie6 = new MovieWithRating(movie1);
+    List<Movie> movies = Arrays.asList(movie1,movie2,movie3,movie4,movie5,movie6);
+
+    @Before
+    public  void setDataToTestMovies(){
+        ((MovieBasic)movie1).setTitle("movie");
+        ((MovieWithRating)movie2).setRate(3);
+        ((MovieWithRating)movie3).setRate(5);
+        ((MovieWithRating)movie7).setRate(2);
+        ((MovieWithRating)movie5).setRate(1);
+        ((MovieWithRating)movie6).setRate(4);
+    }
 
     @Test
     public void sortMoviesByRatingValueTest(){
-
         TopRatedMoviesOfActor sutTopRatedMoviesOfActor = new TopRatedMoviesOfActor(new DataManager());
-
-        Movie movie1 = new MovieBasic();
-        ((MovieBasic)movie1).setTitle("movie");
-
-        Movie movie2 = new MovieWithRating(movie1);
-        ((MovieWithRating)movie2).setRate(3);
-
-        Movie movie3 = new MovieWithRating(movie1);
-        ((MovieWithRating)movie3).setRate(5);
-
-        Movie movie7 = new MovieWithRating(movie1);
-        ((MovieWithRating)movie7).setRate(2);
-        Movie movie4 = new MovieWithGenres(movie7);
-
-        Movie movie8 = new MovieWithGenres(movie1);
-        Movie movie5 = new MovieWithRating(movie8);
-        ((MovieWithRating)movie5).setRate(1);
-
-        Movie movie6 = new MovieWithRating(movie1);
-        ((MovieWithRating)movie6).setRate(4);
-
-        List<Movie> movies = Arrays.asList(movie1,movie2,movie3,movie4,movie5,movie6);
         movies = sutTopRatedMoviesOfActor.sortMoviesByRatingValue(movies);
         assertThat(movies).containsSubsequence(movie3, movie6, movie2, movie4, movie5, movie1);
     }
+
+    @Test
+    public void topXBestRatedMoviesOfActor(){
+        TopRatedMoviesOfActor topRatedMoviesOfActor = new TopRatedMoviesOfActor(new DataManager());
+        List<Movie> result = topRatedMoviesOfActor.topNBestRatedMoviesOfActor(3,movies);
+        assertThat(result).isSortedAccordingTo(new MovieRatingValueComparator());
+        assertThat(result).hasSize(3);
+        assertThat(result).containsSubsequence(movie3, movie6, movie2);
+    }
+
+    @Test
+    public void topXBestRatedMoviesOfActorTest2(){
+        TopRatedMoviesOfActor topRatedMoviesOfActor = new TopRatedMoviesOfActor(new DataManager());
+        List<Movie> result = topRatedMoviesOfActor.topNBestRatedMoviesOfActor(8,movies);
+        assertThat(result).isSortedAccordingTo(new MovieRatingValueComparator());
+        assertThat(result).hasSize(6);
+        assertThat(result).containsSubsequence(movie3, movie6, movie2, movie4, movie5, movie1);
+    }
+
+    @Test
+    public void getNFirstElementsTest(){
+        TopRatedMoviesOfActor topRateMoviesOfActor = new TopRatedMoviesOfActor(new DataManager());
+        List<Movie> result = topRateMoviesOfActor.getNFirstElements(4,movies);
+        assertThat(result).hasSize(4);
+    }
+
 
 }
