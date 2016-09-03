bf297e492e9fac623736cd151071967ee1a66c9a
magdalena <magdalena.ratajczyk77@gmail.com>
2016-09-04 01:33:06 +0200

    Added method: getVotesOfTheHighestRatedMovies and testsEdit: EnumGenre

--- src/main/java/ZZPJ/Project/EnumGenre.java
+++ src/main/java/ZZPJ/Project/EnumGenre.java
@@ -1,13 +1,13 @@
 package ZZPJ.Project;
 
 public enum EnumGenre {
-   Action(1), Adventure(2), Animation(3), Biography(4), Comedy(5), Crime(6), Drama(7), 
-   Family(8), Fantasy(9), FilmNoir(10), History(11), Horror(12), Music(13), Musical(14), 
-   Mystery(15), Romance(16), SciFi(17), Sport(18), Thriller(19), War(20), Western(21);
+   Action("1"), Adventure("2"), Animation("3"), Biography("4"), Comedy("5"), Crime("6"), Drama("7"), 
+   Family("8"), Fantasy("9"), FilmNoir("10"), History("11"), Horror("12"), Music("13"), Musical("14"), 
+   Mystery("15"), Romance("16"), SciFi("17"), Sport("18"), Thriller("19"), War("20"), Western("21");
    
-   int value;
+   public String value;
    
-   EnumGenre(int value) {
-     this.value = value;
+   private EnumGenre(String newValue) {
+     value = newValue;
    }
 }
--- src/test/java/ZZPJ/Project/Model/DataManagerTest.java
+++ src/test/java/ZZPJ/Project/Model/DataManagerTest.java
@@ -5,6 +5,8 @@ import org.jsoup.nodes.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
+import ZZPJ.Project.EnumGenre;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.Format;
@@ -22,229 +24,255 @@ import static org.mockito.Mockito.*;
 
 public class DataManagerTest {
 
-    private String movieTestPath = "src/test/java/ZZPJ/Project/TestFiles/MovieTest.html";
-    private String actorTestPath = "src/test/java/ZZPJ/Project/TestFiles/ActorTest.html";
+  private String movieTestPath = "src/test/java/ZZPJ/Project/TestFiles/MovieTest.html";
+  private String actorTestPath = "src/test/java/ZZPJ/Project/TestFiles/ActorTest.html";
 
-    private Document getDocumenFromFile(String fileName){
-        File file = new File(fileName);
-        Document document = new Document(fileName);
-        try {
-            document = Jsoup.parse(file, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return document;
+  private Document getDocumenFromFile( String fileName ) {
+    File file = new File( fileName );
+    Document document = new Document( fileName );
+    try {
+      document = Jsoup.parse( file, "UTF-8" );
+    } catch ( IOException e ) {
+      e.printStackTrace( );
     }
+    return document;
+  }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void downloadDocumentExceptionTest(){
-        DataManager dataManager = new DataManager();
-        dataManager.downloadDocument("");
-    }
+  @Test( expected = IllegalArgumentException.class )
+  public void downloadDocumentExceptionTest( ) {
+    DataManager dataManager = new DataManager( );
+    dataManager.downloadDocument( "" );
+  }
 
-    @Test
-    public void downloadDocumentTest(){
-        String url = "http://www.imdb.com/name/nm0000288/";
-        DataManager dataManager = new DataManager();
-        assertThat(dataManager.downloadDocument(url)).isNotNull();
-    }
+  @Test
+  public void downloadDocumentTest( ) {
+    String url = "http://www.imdb.com/name/nm0000288/";
+    DataManager dataManager = new DataManager( );
+    assertThat( dataManager.downloadDocument( url ) ).isNotNull( );
+  }
 
-    @Test
-    public void getPageIdMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getPageId(any(Document.class))).thenReturn("tt12345");
-        Document document = new Document("");
-        assertEquals("tt12345",dataManager.getPageId(document));
-    }
+  @Test
+  public void getPageIdMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getPageId( any( Document.class ) ) ).thenReturn( "tt12345" );
+    Document document = new Document( "" );
+    assertEquals( "tt12345", dataManager.getPageId( document ) );
+  }
 
-    @Test
-    public void getPageIdTest(){
-        DataManager dataManager = new DataManager();
-        Document doc = getDocumenFromFile(movieTestPath);
-        assertEquals("tt0482571",dataManager.getPageId(doc));
-    }
+  @Test
+  public void getPageIdTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document doc = getDocumenFromFile( movieTestPath );
+    assertEquals( "tt0482571", dataManager.getPageId( doc ) );
+  }
 
-    @Test
-    public void getMovieTitleMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getMovieTitle(any(Document.class))).thenReturn("Batman");
-        Document document = new Document("");
-        assertEquals("Batman",dataManager.getMovieTitle(document));
-    }
+  @Test
+  public void getMovieTitleMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getMovieTitle( any( Document.class ) ) )
+      .thenReturn( "Batman" );
+    Document document = new Document( "" );
+    assertEquals( "Batman", dataManager.getMovieTitle( document ) );
+  }
 
-    @Test
-    public void getMovieTitleTest(){
-        DataManager dataManager = new DataManager();
-        Document doc = getDocumenFromFile(movieTestPath);
-        assertEquals("Prestiz",dataManager.getMovieTitle(doc));
-    }
+  @Test
+  public void getMovieTitleTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document doc = getDocumenFromFile( movieTestPath );
+    assertEquals( "Prestiz", dataManager.getMovieTitle( doc ) );
+  }
 
-    @Test
-    public void getMovieReleaseYearMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        Date date = new Date();
-        when(dataManager.getMovieReleaseYear(any(Document.class))).thenReturn(date);
-        Document document = new Document("");
-        assertEquals(date,dataManager.getMovieReleaseYear(document));
-    }
+  @Test
+  public void getMovieReleaseYearMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    Date date = new Date( );
+    when( dataManager.getMovieReleaseYear( any( Document.class ) ) )
+      .thenReturn( date );
+    Document document = new Document( "" );
+    assertEquals( date, dataManager.getMovieReleaseYear( document ) );
+  }
 
-    @Test
-    public void getMovieReleaseYearTest(){
-        DataManager dataManager = new DataManager();
-        Document document = getDocumenFromFile(movieTestPath);
-        Format formatter = new SimpleDateFormat("yyyy");
-        Date date = dataManager.getMovieReleaseYear(document);
-        String result = formatter.format(date);
-        assertEquals("2007",result);
-    }
+  @Test
+  public void getMovieReleaseYearTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document document = getDocumenFromFile( movieTestPath );
+    Format formatter = new SimpleDateFormat( "yyyy" );
+    Date date = dataManager.getMovieReleaseYear( document );
+    String result = formatter.format( date );
+    assertEquals( "2007", result );
+  }
 
-    @Test
-    public void getMovieRateMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getMovieRate(any(Document.class))).thenReturn(5.0);
-        Document document = new Document("");
-        assertEquals(5.0,dataManager.getMovieRate(document), 0.0);
-    }
+  @Test
+  public void getMovieRateMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getMovieRate( any( Document.class ) ) ).thenReturn( 5.0 );
+    Document document = new Document( "" );
+    assertEquals( 5.0, dataManager.getMovieRate( document ), 0.0 );
+  }
 
-    @Test
-    public void getMovieRateTest(){
-        DataManager dataManager = new DataManager();
-        Document document = getDocumenFromFile(movieTestPath);
-        assertEquals(8.5, dataManager.getMovieRate(document), 0.0);
-    }
+  @Test
+  public void getMovieRateTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document document = getDocumenFromFile( movieTestPath );
+    assertEquals( 8.5, dataManager.getMovieRate( document ), 0.0 );
+  }
 
-    @Test
-    public void getMovieRatingCountMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getMovieRatingCount(any(Document.class))).thenReturn(5.0);
-        Document document = new Document("");
-        assertEquals(5.0,dataManager.getMovieRatingCount(document), 0.0);
-    }
+  @Test
+  public void getMovieRatingCountMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getMovieRatingCount( any( Document.class ) ) )
+      .thenReturn( 5.0 );
+    Document document = new Document( "" );
+    assertEquals( 5.0, dataManager.getMovieRatingCount( document ), 0.0 );
+  }
 
-    @Test
-    public void getMovieRatingCountTest(){
-        DataManager dataManager = new DataManager();
-        Document document = getDocumenFromFile(movieTestPath);
-        assertEquals(845705, dataManager.getMovieRatingCount(document), 0.0);
-    }
+  @Test
+  public void getMovieRatingCountTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document document = getDocumenFromFile( movieTestPath );
+    assertEquals( 845705, dataManager.getMovieRatingCount( document ), 0.0 );
+  }
 
-    @Test
-    public void getMovieGenresMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        List<String> genres = new ArrayList<String>();
-        genres.add("action");
-        genres.add("biography");
-        when(dataManager.getMovieGenres(any(Document.class))).thenReturn(genres);
-        Document document = new Document("");
-        List<String> result = dataManager.getMovieGenres(document);
-        assertThat(result.get(0)).isEqualTo("action");
-        assertThat(result.get(1)).isEqualTo("biography");
-    }
+  @Test
+  public void getMovieGenresMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    List<String> genres = new ArrayList<String>( );
+    genres.add( "action" );
+    genres.add( "biography" );
+    when( dataManager.getMovieGenres( any( Document.class ) ) ).thenReturn( genres );
+    Document document = new Document( "" );
+    List<String> result = dataManager.getMovieGenres( document );
+    assertThat( result.get( 0 ) ).isEqualTo( "action" );
+    assertThat( result.get( 1 ) ).isEqualTo( "biography" );
+  }
 
-    @Test
-    public void getMovieGenresTest(){
-        DataManager dataManager = new DataManager();
-        Document document = getDocumenFromFile(movieTestPath);
-        List<String> genres = new ArrayList<String>();
-        genres.add("Drama");
-        genres.add("Mystery");
-        genres.add("Sci-Fi");
-        List<String> result = dataManager.getMovieGenres(document);
-        assertThat(result.get(0)).isEqualTo("Drama");
-        assertThat(result.get(1)).isEqualTo("Mystery");
-        assertThat(result.get(2)).isEqualTo("Sci-Fi");
-    }
+  @Test
+  public void getMovieGenresTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document document = getDocumenFromFile( movieTestPath );
+    List<String> genres = new ArrayList<String>( );
+    genres.add( "Drama" );
+    genres.add( "Mystery" );
+    genres.add( "Sci-Fi" );
+    List<String> result = dataManager.getMovieGenres( document );
+    assertThat( result.get( 0 ) ).isEqualTo( "Drama" );
+    assertThat( result.get( 1 ) ).isEqualTo( "Mystery" );
+    assertThat( result.get( 2 ) ).isEqualTo( "Sci-Fi" );
+  }
 
-    @Test
-    public void getActorNameMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getActorName(any(Document.class))).thenReturn("Leonardo DiCaprio");
-        Document document = new Document("");
-        assertEquals("Leonardo DiCaprio",dataManager.getActorName(document));
-    }
+  @Test
+  public void getActorNameMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getActorName( any( Document.class ) ) )
+      .thenReturn( "Leonardo DiCaprio" );
+    Document document = new Document( "" );
+    assertEquals( "Leonardo DiCaprio", dataManager.getActorName( document ) );
+  }
 
-    @Test
-    public void getActorNameTest(){
-        DataManager dataManager = new DataManager();
-        Document doc = getDocumenFromFile(actorTestPath);
-        assertEquals("Leonardo DiCaprio",dataManager.getActorName(doc));
-    }
+  @Test
+  public void getActorNameTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document doc = getDocumenFromFile( actorTestPath );
+    assertEquals( "Leonardo DiCaprio", dataManager.getActorName( doc ) );
+  }
 
-    @Test
-    public void getActorBirthDateMockTest(){
-        DataManager dataManager = mock(DataManager.class);
-        Date date = new Date();
-        when(dataManager.getActorBirthDate(any(Document.class))).thenReturn(date);
-        Document document = new Document("");
-        assertEquals(date,dataManager.getActorBirthDate(document));
-    }
+  @Test
+  public void getActorBirthDateMockTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    Date date = new Date( );
+    when( dataManager.getActorBirthDate( any( Document.class ) ) )
+      .thenReturn( date );
+    Document document = new Document( "" );
+    assertEquals( date, dataManager.getActorBirthDate( document ) );
+  }
 
-    @Test
-    public void getActorBirthDateTest(){
-        DataManager dataManager = new DataManager();
-        Document document = getDocumenFromFile(actorTestPath);
-        Format formatter = new SimpleDateFormat("dd MMMM yyyy", Locale.US);
-        Date date = dataManager.getActorBirthDate(document);
-        String result = formatter.format(date);
-        assertEquals("11 November 1974",result);
-    }
+  @Test
+  public void getActorBirthDateTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document document = getDocumenFromFile( actorTestPath );
+    Format formatter = new SimpleDateFormat( "dd MMMM yyyy", Locale.US );
+    Date date = dataManager.getActorBirthDate( document );
+    String result = formatter.format( date );
+    assertEquals( "11 November 1974", result );
+  }
 
-    @Test
-    public void getActorMoviesLinksMockTest(){
-        List<String> links = new ArrayList<String>();
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getActorMoviesLinks(any(Document.class))).thenReturn(links);
-        assertThat(dataManager.getActorMoviesLinks(new Document(""))).isEqualTo(links);
-    }
+  @Test
+  public void getActorMoviesLinksMockTest( ) {
+    List<String> links = new ArrayList<String>( );
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getActorMoviesLinks( any( Document.class ) ) )
+      .thenReturn( links );
+    assertThat( dataManager.getActorMoviesLinks( new Document( "" ) ) )
+      .isEqualTo( links );
+  }
 
-    @Test
-    public void getActorMoviesLinksTest(){
-        DataManager dataManager = new DataManager();
-        Document doc = getDocumenFromFile(actorTestPath);
-        assertThat(dataManager.getActorMoviesLinks(doc)).hasSize(37);
-    }
+  @Test
+  public void getActorMoviesLinksTest( ) {
+    DataManager dataManager = new DataManager( );
+    Document doc = getDocumenFromFile( actorTestPath );
+    assertThat( dataManager.getActorMoviesLinks( doc ) ).hasSize( 37 );
+  }
 
-    @Test
-    public void getMoviesFromLinksTest(){
-        DataManager dataManager = mock(DataManager.class);
-        when(dataManager.getMoviesFromLinks(anyListOf(String.class),any(Class.class))).thenCallRealMethod();
-        List<Movie> result = dataManager.getMoviesFromLinks(new ArrayList<String>(), MovieBasic.class);
-        assertThat(result).isEmpty();
-    }
+  @Test
+  public void getMoviesFromLinksTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getMoviesFromLinks( anyListOf( String.class ),
+        any( Class.class ) ) ).thenCallRealMethod( );
+    List<Movie> result = dataManager.getMoviesFromLinks( new ArrayList<String>( ),
+        MovieBasic.class );
+    assertThat( result ).isEmpty( );
+  }
 
-    @Test
-    public void getMoviesTest(){
-        DataManager dataManager = mock(DataManager.class);
-        Document doc = getDocumenFromFile(actorTestPath);
-        when(dataManager.getActorMovies(doc,MovieBasic.class)).thenCallRealMethod();
-        when(dataManager.getMoviesFromLinks(anyListOf(String.class),any(Class.class))).thenReturn(new ArrayList<Movie>());
+  @Test
+  public void getMoviesTest( ) {
+    DataManager dataManager = mock( DataManager.class );
+    Document doc = getDocumenFromFile( actorTestPath );
+    when( dataManager.getActorMovies( doc, MovieBasic.class ) )
+      .thenCallRealMethod( );
+    when( dataManager.getMoviesFromLinks( anyListOf( String.class ),
+        any( Class.class ) ) ).thenReturn( new ArrayList<Movie>( ) );
 
-        assertThat(dataManager.getActorMovies(doc,MovieBasic.class)).isEmpty();
-    }
+    assertThat( dataManager.getActorMovies( doc, MovieBasic.class ) ).isEmpty( );
+  }
 
-    @Test
-    public void getBirthDateActorsLinksTest() {
-      DataManager dataManager = new DataManager();
-      assertThat(dataManager.getBirthDateActorsLinks("1994-05-21")).hasSize(2);
-    }
+  @Test
+  public void getBirthDateActorsLinksTest( ) {
+    DataManager dataManager = new DataManager( );
+    assertThat( dataManager.getBirthDateActorsLinks( "1994-05-21" ) ).hasSize( 2 );
+  }
 
-    @Test
-    public void getBirthDateActorsLinksMockTest() {
-      List<String> links = new ArrayList<String>();
-      DataManager dataManager = mock(DataManager.class);
-      when(dataManager.getBirthDateActorsLinks(any(String.class))).thenReturn(links);
-      assertThat(dataManager.getBirthDateActorsLinks(new String(""))).isEqualTo(links);
-    }
-    
-    @Test
-    public void getActorsFromLinksTest() {
-      DataManager dataManager = new DataManager();
-    //  assertThat(dataManager.getActorsFromLinks(dataManager.getBirthDateActorsLinks("1994-05-21")).get(0).getName())
-     //     .isEqualTo("Tom Daley");
-     // assertThat(dataManager.getActorsFromLinks(dataManager.getBirthDateActorsLinks("1994-05-21")).get(1).getName())
-       //   .isEqualTo("Cheyenne Maxey");
-      assertThat(dataManager.getActorsFromLinks(dataManager.getBirthDateActorsLinks("1994-05-21")).size())
-          .isEqualTo(2);
-    }
-    
+  @Test
+  public void getBirthDateActorsLinksMockTest( ) {
+    List<String> links = new ArrayList<String>( );
+    DataManager dataManager = mock( DataManager.class );
+    when( dataManager.getBirthDateActorsLinks( any( String.class ) ) )
+      .thenReturn( links );
+    assertThat( dataManager.getBirthDateActorsLinks( new String( "" ) ) )
+      .isEqualTo( links );
+  }
+
+  @Test
+  public void getActorsFromLinksTest( ) {
+    DataManager dataManager = new DataManager( );
+    assertThat( dataManager
+      .getActorsFromLinks( dataManager.getBirthDateActorsLinks( "1994-05-21" ) )
+      .size( ) ).isEqualTo( 2 );
+  }
+
+  @Test
+  public void getVotesOfTheHighestRatedMoviesTest( ) {
+    DataManager dataManager = new DataManager( );
+    /*
+     * for ( EnumGenre genre : EnumGenre.values( ) ) { assertThat(
+     * dataManager.getVotesOfTheHighestRatedMovies( genre.toString( ) ).size( )
+     * ) .isEqualTo( 50 ); }
+     */
+    assertThat( dataManager.getVotesOfTheHighestRatedMovies( "Action" ).size( ) )
+      .isEqualTo( 50 );
+    assertThat( dataManager.getVotesOfTheHighestRatedMovies( "Drama" ).size( ) )
+      .isEqualTo( 50 );
+    assertThat( dataManager.getVotesOfTheHighestRatedMovies( "Horror" ).size( ) )
+      .isEqualTo( 50 );
+  }
+
 }
