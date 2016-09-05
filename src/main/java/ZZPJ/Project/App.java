ea49fec3dd5ebceec0730f77028bd9549f010d19
adrianw1994 <189809@edu.p.lodz.pl>
2016-09-05 20:15:46 +0200

    Design appearence of UI.

--- src/main/java/ZZPJ/Project/App.java
+++ src/main/java/ZZPJ/Project/App.java
@@ -2,15 +2,81 @@ package ZZPJ.Project;
 
 import javax.swing.*;
 
-public class App extends JFrame{
+public class App extends JFrame {
     private StatisticsMaker statisticsMaker = new StatisticsMaker();
 
     public App() {
         super("IMDB CRAWLER");
+        init();
+    }
+
+    private void init() {
+        setSize(500, 250);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        setBounds();
+
+        add(topRatedMoviesOfActorNumberOfMoviesTextField);
+        add(topRatedMoviesOfActorLabel);
+        add(topRatedMoviesOfActorActorNameTextField);
+        add(topRatedMoviesOfActorButton);
+
+        add(numberOfActorsInAgeRangeLabel);
+        add(numberOfActorsInAgeRangeButton);
+
+        add(genresOfMostPopularMoviesLabel);
+        add(genresOfMostPopularMoviesButton);
+
+        add(actorsBornOnDateLabel);
+        add(actorsBornOnDateTextField);
+        add(actorsBornOnDateButton);
+
+        add(averageNumberOfVotesTopRatedMoviesOfGenreLabel);
+        add(averageNumberOfVotesTopRatedMoviesOfGenreComboBox);
+        add(averageNumberOfVotesTopRatedMoviesOfGenreButton);
+    }
+
+    private void setBounds() {
+        int labelWidth = topRatedMoviesOfActorLabel.getPreferredSize().width;
+        topRatedMoviesOfActorNumberOfMoviesTextField.setBounds(10, 10, 30, 30);
+        topRatedMoviesOfActorLabel.setBounds(50, 10, labelWidth, 30);
+        topRatedMoviesOfActorActorNameTextField.setBounds(labelWidth + 60, 10, 100, 30);
+        topRatedMoviesOfActorButton.setBounds(labelWidth + 170, 10, 50, 30);
+
+        labelWidth = numberOfActorsInAgeRangeLabel.getPreferredSize().width;
+        numberOfActorsInAgeRangeLabel.setBounds(10, 50, labelWidth, 30);
+        numberOfActorsInAgeRangeButton.setBounds(labelWidth + 20, 50, 50, 30);
+
+        labelWidth = genresOfMostPopularMoviesLabel.getPreferredSize().width;
+        genresOfMostPopularMoviesLabel.setBounds(10, 90, labelWidth, 30);
+        genresOfMostPopularMoviesButton.setBounds(labelWidth + 20, 90, 50, 30);
+
+        labelWidth = actorsBornOnDateLabel.getPreferredSize().width;
+        actorsBornOnDateLabel.setBounds(10, 130, labelWidth, 30);
+        actorsBornOnDateTextField.setBounds(labelWidth + 20, 130, 100, 30);
+        actorsBornOnDateButton.setBounds(labelWidth + 130, 130, 50, 30);
+
+        labelWidth = averageNumberOfVotesTopRatedMoviesOfGenreLabel.getPreferredSize().width;
+        averageNumberOfVotesTopRatedMoviesOfGenreLabel.setBounds(10, 170, labelWidth, 30);
+        averageNumberOfVotesTopRatedMoviesOfGenreComboBox.setBounds(labelWidth + 20, 170, 100, 30);
+        averageNumberOfVotesTopRatedMoviesOfGenreButton.setBounds(labelWidth + 130, 170, 50, 30);
     }
 
     public static void main(String[] args) {
-
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new App().setVisible(true);
+            }
+        });
     }
 
     private JTextField topRatedMoviesOfActorNumberOfMoviesTextField = new JTextField("10");
